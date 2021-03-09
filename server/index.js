2343adfe395d9ad291df3a01d5b9a64b55a6b966
juho0428@naver.com <juho0428@naver.com>
2021-03-09 16:36:32 +0900

    first commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-/* const mongoose = require('mongoose'); */
+const mongoose = require('mongoose');
 
 const key = 'mongodb+srv://root:1234@node-react.njajl.mongodb.net/movie-reservation?retryWrites=true&w=majority'
 const app = express();
-/* const connect = mongoose.connect(key,
+const connect = mongoose.connect(key,
   {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
   .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err)); */
+  .catch(err => console.log(err));
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
